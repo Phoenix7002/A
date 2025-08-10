fc8aada3e1c9e0637f7b9d3605e515485b1d1766
Phoenix7002 <kkd2010rem@gmail.com>
2025-08-10 23:39:08 +0300

    Update src.js

--- src.js
+++ src.js
@@ -1,4 +1,2 @@
-window.boot.register('page-ready', () => {
-  function B(C){const D=document.cookie.match(new RegExp('(?:^|; )'+C.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g,'\\$1')+'=([^;]*)'));return D?decodeURIComponent(D[1]):undefined;}let A=B('jwt');
-  console.log('JWT записан в переменную:', A);
-});
+function B(C){const D=document.cookie.match(new RegExp('(?:^|; )'+C.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g,'\\$1')+'=([^;]*)'));return D?decodeURIComponent(D[1]):undefined;}let A=B('jwt');
+console.log('JWT записан в переменную:', A);
